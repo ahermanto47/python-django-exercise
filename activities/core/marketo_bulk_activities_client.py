342d11ee682c2cb6263799a57eb5783f85372f6c
Andi Hermanto <ahermanto47@gmail.com>
2022-07-15 22:32:28 -0400

    Update tests

--- activities/core/marketo_bulk_activities_client.py
+++ activities/core/marketo_bulk_activities_client.py
@@ -1,53 +1,51 @@
 import requests
 
-baseUrl="http://127.0.0.1:4010"
-
 class GetBulkExportActivitiesClient:
 
     """A simple console application to create activities job, 
        poll its status, and retrieve the file when status completed"""
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self,baseUrl) -> None:
+        self.baseUrl=baseUrl
     
     """Pull all export jobs"""
 
     def get_all_export_jobs(self):
         path = '/bulk/v1/activities/export.json'
-        response = requests.get(url=baseUrl+path)
+        response = requests.get(url=self.baseUrl+path)
         return response
 
     """Create an export jobs"""
 
     def create_export_job(self,data):
         path = '/bulk/v1/activities/export/create.json'
-        response = requests.post(url=baseUrl+path, json=data)
+        response = requests.post(url=self.baseUrl+path, json=data)
         return response
 
     """Cancel an export jobs"""
 
     def cancel_export_job(self,export_id):
         path = f'/bulk/v1/activities/export/{export_id}/cancel.json'
-        response = requests.post(url=baseUrl+path)
+        response = requests.post(url=self.baseUrl+path)
         return response
 
     """Enqueue an export jobs"""
 
     def enqueue_export_job(self,export_id):
         path = f'/bulk/v1/activities/export/{export_id}/enqueue.json'
-        response = requests.post(url=baseUrl+path)
+        response = requests.post(url=self.baseUrl+path)
         return response
 
     """Get status of an export job"""
 
     def get_export_job_status(self,export_id):
         path = f'/bulk/v1/activities/export/{export_id}/status.json'
-        response = requests.get(url=baseUrl+path)
+        response = requests.get(url=self.baseUrl+path)
         return response
 
     """Get file of an export job"""
 
     def get_export_file(self,export_id):
         path = f'/bulk/v1/activities/export/{export_id}/file.json'
-        response = requests.get(url=baseUrl+path)
+        response = requests.get(url=self.baseUrl+path)
         return response
--- activities/core/test_marketo_bulk_activities_client.py
+++ activities/core/test_marketo_bulk_activities_client.py
@@ -3,40 +3,49 @@ import pytest
 
 @pytest.fixture
 def client():
-    return GetBulkExportActivitiesClient()
+    return GetBulkExportActivitiesClient(baseUrl="http://127.0.0.1:4010")
 
 def test_get_all_export_jobs_gets_response_with_200_code(client):
     response = client.get_all_export_jobs()
     assert response.status_code == 200
+    response_data = response.json()
+    assert len(response_data['result']) >= 0
 
 def test_create_export_job(client):
     payload = {
         'fields': ['leadId'],
         'filter': {
             'createdAt': {
-                'startAt': '2017-01-01T00:00:00Z',
-                'endAt': '2017-01-31T00:00:00Z'
+                'startAt': '2022-07-01T00:00:00Z',
+                'endAt': '2022-07-31T00:00:00Z'
             }
         }
     }
     response = client.create_export_job(data=payload)
     assert response.status_code == 200
     response_data = response.json()
+    assert len(response_data['result']) >= 0
     assert response_data['requestId'] != None
 
 def test_cancel_export_job(client):
     response = client.cancel_export_job(export_id='abc123')
     assert response.status_code == 200
+    response_data = response.json()
+    assert response_data['requestId'] != None
 
 def test_enqueue_export_job(client):
     response = client.enqueue_export_job(export_id='abc123')
     assert response.status_code == 200
+    response_data = response.json()
+    assert response_data['requestId'] != None
 
 def test_get_export_job_status(client):
     response = client.get_export_job_status(export_id='abc123')
     assert response.status_code == 200
+    response_data = response.json()
+    assert response_data['requestId'] != None
 
 def test_get_export_file(client):
     response = client.get_export_file(export_id='abc123')
     assert response.status_code == 200
-    
+
