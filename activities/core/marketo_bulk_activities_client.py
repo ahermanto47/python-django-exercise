35eaef81369b4bfab45c18d0c0aef6970dc3e8c7
Andi Hermanto <ahermanto47@gmail.com>
2022-07-16 08:16:05 -0400

    Refactor 1

--- activities/core/marketo_bulk_activities_client.py
+++ activities/core/marketo_bulk_activities_client.py
@@ -1,4 +1,13 @@
 import requests
+from enum import Enum
+
+class Operation(Enum):
+    get_all_job = '/bulk/v1/activities/export.json'
+    get_job_status = f'/bulk/v1/activities/export/{{export_id}}/status.json'
+    get_export_file = f'/bulk/v1/activities/export/{{export_id}}/file.json'
+    create_job = '/bulk/v1/activities/export/create.json'
+    cancel_job = f'/bulk/v1/activities/export/{{export_id}}/cancel.json'
+    enqueue_job = f'/bulk/v1/activities/export/{{export_id}}/enqueue.json'
 
 class GetBulkExportActivitiesClient:
 
@@ -7,45 +16,44 @@ class GetBulkExportActivitiesClient:
 
     def __init__(self,baseUrl) -> None:
         self.baseUrl=baseUrl
+
+    def do_get(self,path):
+        return requests.get(url=self.baseUrl+path)
     
+    def do_post(self,path,data=None):
+        if data != None :
+            return requests.post(url=self.baseUrl+path,json=data) 
+        else:
+            return requests.post(url=self.baseUrl+path)
+
     """Pull all export jobs"""
 
     def get_all_export_jobs(self):
-        path = '/bulk/v1/activities/export.json'
-        response = requests.get(url=self.baseUrl+path)
-        return response
-
-    """Create an export jobs"""
-
-    def create_export_job(self,data):
-        path = '/bulk/v1/activities/export/create.json'
-        response = requests.post(url=self.baseUrl+path, json=data)
-        return response
-
-    """Cancel an export jobs"""
-
-    def cancel_export_job(self,export_id):
-        path = f'/bulk/v1/activities/export/{export_id}/cancel.json'
-        response = requests.post(url=self.baseUrl+path)
-        return response
-
-    """Enqueue an export jobs"""
-
-    def enqueue_export_job(self,export_id):
-        path = f'/bulk/v1/activities/export/{export_id}/enqueue.json'
-        response = requests.post(url=self.baseUrl+path)
-        return response
+        return self.do_get(path=Operation.get_all_job.value)
 
     """Get status of an export job"""
 
     def get_export_job_status(self,export_id):
-        path = f'/bulk/v1/activities/export/{export_id}/status.json'
-        response = requests.get(url=self.baseUrl+path)
-        return response
+        return self.do_get(path=Operation.get_job_status.value)
 
     """Get file of an export job"""
 
     def get_export_file(self,export_id):
-        path = f'/bulk/v1/activities/export/{export_id}/file.json'
-        response = requests.get(url=self.baseUrl+path)
-        return response
+        return self.do_get(path=Operation.get_export_file.value)
+
+    """Create an export jobs"""
+
+    def create_export_job(self,data):
+        return self.do_post(path=Operation.create_job.value,data=data)
+
+    """Cancel an export jobs"""
+
+    def cancel_export_job(self,export_id):
+        return self.do_post(path=Operation.cancel_job.value)
+
+    """Enqueue an export jobs"""
+
+    def enqueue_export_job(self,export_id):
+        return self.do_post(path=Operation.enqueue_job.value)
+
+
